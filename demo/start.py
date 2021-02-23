e0baab3c53707c4b64cd12a20a26fab25c4f7392
king <2075259561@qq.com>
2021-02-23 17:07:35 +0800

    python

---
+++ demo/start.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+if __name__ == '__main__':
+    num = 10
+    i = -1
+    x = 'A'
+    y = "B"
+    str = 'hellowWorld'
+    str1 = 'myWorld'
+    tuple = ('a', 'ab', 'abc', 'abcd')
+    tuple1 = ('a', 'b', 'c', 'd')
+
+if (num < 5):
+    print('hellow world!')
+else:
+    print('false')
+
+print x
+print y
+
+print x,
+print y,
+
+if (i < 0):
+    print('<')
+elif (i == 0):
+    print('=')
+else:
+    print('>')
+
+print str[1:5]
+print str[0]
+print str[1:]
+print (str + ',') * 8
+print str + str1
+print tuple + tuple1
